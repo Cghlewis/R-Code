4214ccd3c2a61770484c4ad8846ea0d0ccb11251
Cghlewis <33465957+Cghlewis@users.noreply.github.com>
2018-03-05 11:43:12 -0600

    Add files via upload

---
+++ R presentation part 2.R
@@ -0,0 +1,106 @@
+#Let's view our data
+head(data8,10)
+colnames(data8)
+
+#What types of variables do we have
+str(data8)
+#Dates look weird so let's convert those back to dates
+
+#Can use this if dates come in as numbers
+
+data4$StartDate2<-convertToDate(data4$StartDate)
+data4$EndDate2<-convertToDate(data4$EndDate)
+
+
+#Use this if dates come in as strings
+data8$StartDate3<-as.Date(data8$StartDate)
+data8$EndDate3<-as.Date(data8$EndDate)
+
+#Now let's drop the old dates
+newdata <- data8[c(-1:-2,-18:-19)]
+head(newdata)
+
+#Let's drop unnecessary vars
+newdata$V10<-NULL
+newdata$Q1<-NULL
+newdata$Q3<-NULL
+
+#Let's rename the variables
+install.packages("plyr")
+library(plyr)
+newdata<-rename(newdata, c("Q4"="SchoolID", "Q5"="Age", "Q6"="Gender", "Q7"="Race", "Q7_TEXT"="Race_Text",
+                  "Q8"="Grade"))
+head(newdata)
+
+#Recode Gender
+install.packages("car")
+library(car)
+
+newdata$Female<-recode(newdata$Gender,"1=0;2=1")
+table(newdata$Female, newdata$Gender)
+
+##OR
+newdata$Female2<-ifelse(newdata$Gender == 1, 0, ifelse(newdata$Gender == 2, 1, 3))
+table(newdata$Female2, newdata$Gender)
+
+##OR
+newdata$Female3[newdata$Female2==0] <- "Male"
+newdata$Female3[newdata$Female2==1] <- "Female"
+
+#Reverse code 18_1
+
+newdata$Q18_1new<-((max(newdata$Q18_1)+1)-newdata$Q18_1)
+table(newdata$Q18_1, newdata$Q18_1new)
+##If someone didn't get the max score, this formula will not work (you will need to say 11+1 instead)
+
+#Sum scores
+
+newdata$Stresssum <- newdata$Q18_1new + newdata$Q18_2 + newdata$Q18_3 + newdata$Q18_4 +
+  newdata$Q18_5
+
+##OR
+colnames(newdata)
+
+newdata$Stresssum2<- rowSums(newdata[ , c(9:12,14)]) 
+
+#Stats for sum score
+mean(newdata$Stresssum)
+sd(newdata$Stresssum)
+range(newdata$Stresssum)
+install.packages("psych")
+library(psych)
+
+describe(newdata$Stresssum)
+
+describeBy(newdata$Stresssum, group=newdata$Female3)
+
+#Graphs
+hist(newdata$Stresssum)
+
+plot(newdata$Q18_1new,newdata$Q18_2)
+
+library(lattice)
+
+histogram(newdata$Stresssum)
+histogram(~newdata$Stresssum|newdata$Female3)
+
+
+
+
+
+
+xyplot(x~y)
+
+xyplot(x~y|z)
+
+ggplot(cardb,aes(x=mileage,y=price, color=make, shape=make, size=liter))+geom_point() +
+  labs(color="Make", x="MIles per gallon", y="Selling price")  #could say shape=make
+
+ggplot(cardb, aes(x=price)) + geom_histogram
+
+ggplot(cardb, aes(x=price, fill=make)) + geom_histogram(bins=80) #fill changes colors based on make
+
+
+
+
+
---
+++ R presentation.R
@@ -0,0 +1,123 @@
+##########Using R for data management##############
+
+###Download R studio
+
+##Hashtags!!
+##Comment everything you do
+
+#This is all using a pc, things are different for mac
+
+###Extras
+
+library(foreign)
+
+readLines("C:/Users/hamptoncg/Desktop/R/Teacher_Self_Report.csv", 
+          n=10)
+
+##Using sweep to clear environment
+#Clear console
+
+
+#####################################Getting your data into R#############################################
+
+##Set your working directory so you don't have to write that every time
+##Use forward slashes
+##R is case sensitive
+#Use sweep below to clear out Global Environment
+#Use Edit--Clear Console to clear console
+
+setwd("C:/Users/hamptoncg/Desktop/R")
+
+##Read data into R and assign it to a "data frame", otherwise it will read in data but you can't access it
+
+#Read.table can be used to load in any basic text file
+
+#Typing read.table into console to get all options
+
+#txt
+
+data<-read.table("Comma Separated TXT.txt", header = FALSE, sep=",")
+
+#dat
+
+data2<-read.table("Tab Delimited DAT.dat", sep="\t", header=FALSE)
+
+#csv
+data3<-read.table("CSV.csv", sep=",",header=TRUE)
+
+  #Could also use read.csv
+sample<-read.csv("CSV.csv")
+
+  #There are TOO many ways to install the same file
+
+####install.packages("readr")
+####library(readr)
+
+####sample2<-read_csv("CSV.csv")
+
+###For files that have any formatting you can't use read.table. You'll need some sort of package.
+
+#xlsx
+
+  #Install packages to help load data
+
+install.packages("openxlsx")
+library(openxlsx)
+
+  #Typing read.xlsx into console to get all options
+
+data4<-read.xlsx("XLSX two row.xlsx", sheet=1, startRow =2, colNames=TRUE)
+str(data4)
+
+  #Dates come in as strings this way
+  #Can convert back to date using this
+
+data4$StartDate2<-convertToDate(data4$StartDate)
+data4$EndDate2<-convertToDate(data4$EndDate)
+
+
+#spss, sas, stata, you can use the "foreign" package
+
+install.packages("foreign")
+
+
+data12<-read.spss("SPSS.sav", to.data.frame=T, use.value.labels=FALSE)
+str(data6)
+str(data12)
+
+##Bad thing about foreign is that it brings strings in as factors which you may not want
+
+#Haven is how R loads in data if you use the file--import dataset drop down menu
+library(haven)
+SPSS <- read_sav("C:/Users/hamptoncg/Desktop/R/SPSS.sav")
+View(SPSS)
+
+str(SPSS)
+
+##There is one package that will allow you to load in all data types the same way, Rio
+
+install.packages("rio")
+library(rio)
+
+#txt
+
+data7<-import("Comma Separated TXT.txt")
+
+#xlsx
+##Date actually loads in correctly! 
+data8<-import("XLSX two row.xlsx", skip=1)
+str(data8)
+
+#SPSS
+#Rio will load your strings in as strings
+data9<-import("SPSS.sav")
+str(data9$TeacherID)
+str(data9)
+#Looks a lot cleaner
+#Structure will look weird for SPSS b/c it brings in all the SPSS formatting but it's ok
+
+####install.packages("Hmisc")
+###library(Hmisc)
+####mydata <- spss.get("SpSS.sav", use.value.labels=TRUE)
+
+
